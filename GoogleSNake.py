328d1ba6a1fb8c1daa5e15aa07f5ff989b1e9289
Sarmad Jabri <114747536+sarmadjabri@users.noreply.github.com>
2024-06-17 09:39:05 -0400

    Update GoogleSNake.py

--- GoogleSNake.py
+++ GoogleSNake.py
@@ -28,7 +28,6 @@ class SnakeGame:
         return state
 
     def step(self, action):
-        # Update snake direction
         if action == 0:  # Up
             self.direction = (0, -1)
         elif action == 1:  # Down
@@ -54,8 +53,15 @@ class SnakeGame:
             self.score += 1
             return self._get_state(), 10, False, {"score": self.score, "reward": 10}
         else:
-            self.snake_position.pop()
-            return self._get_state(), 0, False, {"score": self.score, "reward": 0}
+            # Reward for moving closer to food
+            food_distance = abs(new_head_x - self.food_position[0]) + abs(new_head_y - self.food_position[1])
+            previous_distance = abs(self.snake_position[0][0] - self.food_position[0]) + abs(self.snake_position[0][1] - self.food_position[1])
+            if food_distance < previous_distance:
+                reward = 0.1
+            else:
+                reward = -0.1
+            self.snake_position.pop()  # Remove the tail
+            return self._get_state(), reward, False, {"score": self.score, "reward": reward}
 
     def reset(self):
         self.snake_position = [(0, 0)]
@@ -74,7 +80,7 @@ class DQNAgent:
         self.action_size = action_size
         self.memory = []
         self.gamma = 0.95
-        self.epsilon = 0.1  # Lower epsilon for more exploitation
+        self.epsilon = 0.5  # Lower epsilon for more exploitation
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
@@ -119,7 +125,7 @@ class DQNAgent:
 if __name__ == "__main__":
     game = SnakeGame()
     agent = DQNAgent(state_size=10, action_size=4)
-    batch_size = 512  # Increased batch size
+    batch_size = 1024  # Increased batch size
     episodes = 100000000000
 
     for episode in range(episodes):
